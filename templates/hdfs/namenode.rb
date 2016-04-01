c48a1cf96312173cd9f5592128777f4322bf9b03
Alex Chistyakov <alexclear@gmail.com>
2016-04-01 16:26:28 +0300

    Use uid instead of sudo

--- templates/hdfs/namenode.rb
+++ templates/hdfs/namenode.rb
@@ -3,10 +3,11 @@ Eye.application 'hdfs-namenode-{{ env_name }}' do
   stdall '/var/log/eye/hdfs-namenode-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  uid "{{ hadoop_user }}"
 
   process :namenode_{{ env_name }} do
     pid_file '{{ hadoop_var_prefix }}/hdfs_namenode.pid'
-    start_command 'sudo -u {{ hadoop_user }} {{ hadoop_distr_prefix }}/bin/hdfs namenode'
+    start_command '{{ hadoop_distr_prefix }}/bin/hdfs namenode'
 
     daemonize true
     start_timeout 10.seconds
